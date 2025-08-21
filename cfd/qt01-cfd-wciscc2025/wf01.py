fbc9103f4df70352e6d89a0cd493b264bfdc1812
Andy Gallo <agallojr@518computing.com>
2025-08-21 14:47:03 -0400

    qt01 cfd - first iteration

--- cfd/qt01-cfd-wciscc2025/wf01.py
+++ cfd/qt01-cfd-wciscc2025/wf01.py
@@ -9,57 +9,87 @@ import sys
 sys.path.append("wciscc2025/qlsa")
 
 import tomllib
-import pprint
+from pathlib import Path
+
+from lwfm.base.Workflow import Workflow
+from lwfm.base.JobDefn import JobDefn
+from lwfm.midware.LwfManager import lwfManager, logger
+from lwfm.base.JobStatus import JobStatus
 
 if __name__ == '__main__':
+    # load the TOML file of test case inputs
     try:
         with open("./wf01-in.toml", "rb") as f:
             data = tomllib.load(f)
-        pprint.pprint(data)
     except FileNotFoundError:
         print("Error: wf01-in.toml not found.")
+        sys.exit(1)
     except tomllib.TOMLDecodeError as e:
         print(f"Error decoding TOML: {e}")
+        sys.exit(1)
+    globalArgs = data["global"]
 
-    # wf = Workflow("HHL Circuit Example", "Example of running HHL circuit with a CFD case file.")
+    # make a workflow to bundle all these cases
+    wf = Workflow("HHL size variations",
+        "Fixing most params, varying matrix size - grid resolution & qubits - on various backends",
+        {})    # TODO: what workflow-level metadata is useful here?
+    wf = lwfManager.putWorkflow(wf)
+    logger.setContext(wf)
+    # modify the output directory name to include the workflow ID
+    globalArgs["savedir"] = globalArgs["savedir"] + "/" + str(wf.getWorkflowId())
 
-    # # we'll run the first part of the ORNL instructions as is, here, in a sandbox
-    # # using their library dependencies
-    # site = lwfManager.getSite("local")
+    # for each case in the workflow toml
+    for caseName, caseArgs in ((k, v) for k, v in data.items() if k != "global"):
+        # get the args for this case and merge in the global args
+        caseArgs.update(globalArgs)
+        caseArgs["case_name"] = caseName
 
-    # # Create JobDefn to generate the circuit using circuit_HHL.py run here, synchronously
-    # # the input_vars.yaml file can be anywhere, represents your engineering "case"
-    # entry_point = "python wciscc2025/qlsa/circuit_HHL.py"
-    # args = ["-case", "sample-tridiag", "-casefile", "wciscc2025/qlsa/input_vars.yaml", "--savedata"]
-    # jobDefn = JobDefn(entry_point, JobDefn.ENTRY_TYPE_SHELL, args)
-    # status = site.getRunDriver().submit(jobDefn, wf)
-    # if status is None:
-    #     raise RuntimeError("Job submission failed")
-    # status = lwfManager.wait(status.getJobId())
+        # take the templatized ORNL input_vars.yaml, fill it in with the case args, save it
+        with open("./input_vars.yaml", "r", encoding="utf-8") as f:
+            input_vars = f.read()
+        for key, value in caseArgs.items():
+            input_vars = input_vars.replace("$" + key, str(value))
+        out_dir = Path(globalArgs["savedir"])
+        out_dir.mkdir(parents=True, exist_ok=True)
+        out_path = out_dir.joinpath(f"input_vars_{caseName}.yaml")
+        circuit_qpy_path = out_dir.joinpath(f"{caseName}_circ_nqmatrix{caseArgs['NQ_MATRIX']}.qpy")
+        with open(out_path, "w", encoding="utf-8") as f:
+            f.write(input_vars)
 
-    # # the ORNL code is going to make a ./models/ dir containing a circuit in QPY format,
-    # # and we've already seen how to deal with that in prior examples
-    # # their executor, wciscc2025/qlsa/solver.py, will accept some arguments like the
-    # # number of shots, noise info about the target backend simulator, etc.
-    # # it uses numpy to solve the equations classically for comparison, and we'll not
-    # # do that part, so here's the meat of the matter:
-    # #    matrix, vector, input_vars = matvec.get_matrix_vector(args)
-    # #    n_qubits_matrix = int(np.log2(matrix.shape[0]))
-    # #    "decide if you want a simulator or a real backend from IBM or IQM" - we'll ignore IQM
-    # #    qc_circ(n_qubits_matrix, classical_solution, args, input_vars)
-    # # this last function decides if we're using Aer simulator or a real backend,
-    # # loads the circuit from qpy format, transpiles the circuit accordingly
-    # # (see qlsa/func_qc.py), runs the circuit with some twists, and saves the results to a file
-    # # if you're good being pinning to their library dependencies, just use their code,
-    # # but otherwise we can use the lwfm ability to define a site in a virtual environment,
-    # # then look at their code (again, func_qc.py) and decide which of the "twists" are needed.
-    # site = lwfManager.getSite("ibm-quantum-venv")
-    # computeType = "statevector_sim_aer" # for simplicity of the example
-    # runArgs = {"shots": 1024}
-    # jobDefn = JobDefn("models/sample_HHL_circ_nqmatrix1.qpy",
-    #                   JobDefn.ENTRY_TYPE_STRING, {"format": "qpy"})
-    # status = site.getRunDriver().submit(jobDefn, wf, computeType, runArgs)
-    # if status is None:
-    #     raise RuntimeError("Job submission failed")
-    # status = lwfManager.wait(status.getJobId())
-    # print(lwfManager.deserialize(status.getNativeInfo()))  # type: ignore
+        # various parts to each case - circuit generation/preprocessing, execution, post processing
+        # case preprocess step
+        # TODO vary the 'sample-tridiag'
+        preprocess_site = lwfManager.getSite("local")
+        preprocess_status = preprocess_site.getRunDriver().submit(
+            JobDefn("python wciscc2025/qlsa/circuit_HHL.py", JobDefn.ENTRY_TYPE_SHELL,
+                ["-case", "sample-tridiag", "-casefile", str(out_path), "--savedata"]),
+                wf
+        )
+        if (preprocess_status is None):
+            logger.error(f"Preprocess job submission failed {caseName}")
+            continue
+        preprocess_status = lwfManager.wait(preprocess_status.getJobId())
+        if (preprocess_status is None) or (preprocess_status.getStatus() != JobStatus.COMPLETE):
+            logger.error(f"Preprocess job failed {caseName}")
+            continue
+
+        # case execution step
+        exec_site = lwfManager.getSite("ibm-quantum-venv")
+        computeType = "statevector_sim_aer"     # TODO make this an arg to the workflow / case
+        runArgs = {"shots": 1024}               # TODO make this an arg to the workflow / case
+        jobDefn = JobDefn(str(circuit_qpy_path), JobDefn.ENTRY_TYPE_STRING, {"format": "qpy"})
+        exec_status = exec_site.getRunDriver().submit(jobDefn, wf, computeType, runArgs)
+        if exec_status is None:
+            logger.error(f"Circuit execution job submission failed {caseName}")
+            continue
+        exec_status = lwfManager.wait(exec_status.getJobId())    # TODO make async
+        if (exec_status is None) or (exec_status.getStatus() != JobStatus.COMPLETE):
+            logger.error(f"Circuit execution job failed {caseName}")
+            continue
+
+        # case postprocess step
+        result = lwfManager.deserialize(exec_status.getNativeInfo())  # type: ignore
+        logger.info(f"Circuit execution job completed {caseName}: {result}")
+
+    # workflow postprocess step
+    # TODO
