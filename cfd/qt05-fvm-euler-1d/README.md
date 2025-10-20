c875a403fb46347c76407aa41c24471fc99efad1
Andy Gallo <agallojr@518computing.com>
2025-10-20 19:25:28 -0400

    qt05 fvm

---
+++ cfd/qt05-fvm-euler-1d/README.md
@@ -0,0 +1,4 @@
+
+Uses the requirements.txt from ORNL WCISCC2025.
+Uses the fvm_euler_1d_solver code based partially on the above.
+Wraps it with the lwfm workflow.
---
+++ cfd/qt05-fvm-euler-1d/setup.py
@@ -0,0 +1,66 @@
+"""
+help install dependencies
+"""
+
+import os
+import sys
+import subprocess
+from setuptools import setup
+
+# Clone the fvm euler 1d repository if it doesn't exist
+git_user_key = os.environ.get("GIT_USER_KEY")
+if not git_user_key:
+    # You must set this environment variable for the installation to work
+    raise ValueError("GIT_USER_KEY environment variable is not set")
+REPO_URL = "https://{git_user_key}@github.com/mhawwary/fvm_euler_1d_solver"
+REPO_DIR = "fvm_euler_1d_solver"
+if not os.path.exists(REPO_DIR):
+    print(f"Cloning {REPO_URL}...")
+    subprocess.check_call(["git", "clone", REPO_URL])
+    print(f"Successfully cloned {REPO_URL} to {REPO_DIR}")
+else:
+    print(f"Found existing {REPO_DIR} directory")
+
+
+# Clone the WCISCC2025 repository if it doesn't exist
+REPO_URL = "https://github.com/olcf/wciscc2025"
+REPO_DIR = "wciscc2025"
+if not os.path.exists(REPO_DIR):
+    print(f"Cloning {REPO_URL}...")
+    subprocess.check_call(["git", "clone", REPO_URL])
+    print(f"Successfully cloned {REPO_URL} to {REPO_DIR}")
+else:
+    print(f"Found existing {REPO_DIR} directory")
+
+# Parse requirements from file
+# notice here no dependency on qiskit - its all in the qlsa requirements.txt, meaning,
+# its their dependent version of qiskit etc
+req_file = os.path.join(REPO_DIR, 'qlsa', 'requirements.txt')
+dependencies = []
+
+if os.path.exists(req_file):
+    with open(req_file) as f:
+        for line in f:
+            line = line.strip()
+            if line and not line.startswith('#'):
+                dependencies.append(line)
+    print(f"Found {len(dependencies)} dependencies in {req_file}")
+else:
+    print(f"Warning: {req_file} not found!")
+
+extra_depends = [
+    "lwfm @ git+https://github.com/lwfm-proj/lwfm@develop",  # use develop branch faster bug fixes
+    "setuptools",
+    "numpy",
+    "scipy",
+    "toml"
+]
+
+dependencies.extend(extra_depends)
+
+# Dynamic dependency configuration
+setup(
+    packages=['qt01_cfd_wciscc2025'],
+    package_dir={'': '.'},
+    install_requires=dependencies
+)
